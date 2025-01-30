d96d20f7e7b527501669beec75ac93dc435adb11
AdhithyanAsus <adhithyanktd@gmail.com>
2025-01-30 22:38:11 +0530

    Added icon to login button in the index page. Created admin side index page

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Admin Panel</h1>
+    <nav>
+        <ul>
+            <li><a href="rooms.php">Rooms</a></li>
+            <li><a href="users.php">Users</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
