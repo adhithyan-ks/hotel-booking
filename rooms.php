916fdab65750d6f0946af5523695522c3bbfd880
AdhithyanAsus <adhithyanktd@gmail.com>
2025-01-31 14:52:25 +0530

    changed user side room.html to php format and added room list

---
+++ rooms.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styles/nav.css" />
+    <style>
+        * {
+            font-family: Arial, sans-serif;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+    </style>
+    <title>Rooms</title>
+  </head>
+
+  <body>
+    <header>
+      <h1 class="logo">PalmHotel</h1>
+      <nav>
+        <ul class="nav-links">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="rooms.php">Rooms</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="contact.html">Contact</a></li>
+          <li><a href="about.html">About</a></li>
+        </ul>
+      </nav>
+      <a class="cta" href="login.html"><button>Login</button></a>
+    </header>
+
+    <main class="container">
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "hotel-booking";
+    //Ceate connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    //Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    //Select all records from the users table
+    $query = "SELECT * FROM rooms";
+    $result = $conn->query($query);
+    
+    echo "<hr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<div class='room'>";
+        echo "<p>Room ID:" . $row['room_id'] . "</p>";
+        echo "<p>Room Type:" . $row['room_type'] . "</p>";
+        echo "<p>Description" . $row['description'] . "</p>";
+        echo "<p>Price per night" . $row['price_per_night'] . "</p>";
+        echo "<p>Availability Status" . $row['avail_status'] . "</p>";
+        echo "<p>Image:" . $row['image_url'] . "</p><hr>";
+    }
+    echo "</table>";
+    ?>
+    <!-- Footer section 
+    <footer>
+      <p>&copy; 2024 Hotel Name. All rights reserved.</p>
+    </footer>
+  --></body></main>
+</html>
